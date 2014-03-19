b219c3f10b085da9d59db0bca3dfdd8fc33cd637
Leonel Santos <leonelsantos.me@gmail.com>
2014-03-18 21:22:05 -0500

    Skip locale validation, silence I18n.enforce_available_locales deprecation warning when running spec

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,10 @@ module TodoRailsTest
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = "1.0"
+
+    # Skip locale validation and silence the following warning when running rspec:
+    # [deprecated] I18n.enforce_available_locales will default to true in the future. 
+    # If you really want to skip validation of your locale you can set I18n.enforce_available_locales = false to avoid this message.
+    config.i18n.enforce_available_locales = false
   end
 end
