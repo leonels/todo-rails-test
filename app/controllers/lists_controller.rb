7108cc14a385dde25ff0f1731376a5b760237868
Leonel Santos <leonelsantos.me@gmail.com>
2014-03-18 20:53:25 -0500

    Update the ListsController so that every action responds to JSON

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,31 +2,55 @@ class ListsController < ApplicationController
   # GET /lists
   def index
     @lists = List.all
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @lists }
+    end
   end
 
   # GET /lists/1
   def show
     @list = List.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @list }
+    end
   end
 
   # GET /lists/new
   def new
     @list = List.new
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @list }
+    end
   end
 
   # GET /lists/1/edit
   def edit
     @list = List.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @list }
+    end
   end
 
   # POST /lists
   def create
     @list = List.new(params[:list])
 
-    if @list.save
-      redirect_to @list, notice: 'List was successfully created.'
-    else
-      render action: "new"
+    respond_to do |format|
+      if @list.save
+        format.html { redirect_to @list, notice: 'List was successfully created.' }
+        format.json { render json: @list, status: :created, location: @list }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @list.errors, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -34,10 +58,14 @@ class ListsController < ApplicationController
   def update
     @list = List.find(params[:id])
 
-    if @list.update_attributes(params[:list])
-      redirect_to @list, notice: 'List was successfully updated.'
-    else
-      render action: "edit"
+    respond_to do |format|
+      if @list.update_attributes(params[:list])
+        format.html { redirect_to @list, notice: 'List was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @list.errors, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -46,6 +74,9 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
     @list.destroy
 
-    redirect_to lists_url
+    respond_to do |format|
+      format.html { redirect_to lists_url }
+      format.json { head :no_content }
+    end
   end
 end
