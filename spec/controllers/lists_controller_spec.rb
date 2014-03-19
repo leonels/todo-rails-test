388ee4d34a01c019a1ae1bfc9d9ba97b6e916a77
Leonel Santos <leonelsantos.me@gmail.com>
2014-03-18 21:55:23 -0500

    added simple rspec tests for ListsController: index, show and edit actions should respond to JSON

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe ListsController do
+	describe "GET #index JSON" do
+	  it "Lists index action should respond to JSON" do
+	    get :index, :format => :json
+	    response.should be_success
+	  end
+	end
 
+	describe "GET #show JSON" do
+    list = List.new(title: "To-Do Rails Test")
+    list.save
+	  
+    it "Lists show action should respond to JSON" do
+	    get :show, id: list.id, :format => :json
+	    response.should be_success
+	  end
+
+    it "Lists edit action should respond to JSON" do
+      get :edit, id: list.id, :format => :json
+      response.should be_success
+    end
+
+	end
 end
